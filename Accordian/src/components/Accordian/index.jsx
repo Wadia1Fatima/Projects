16e0909b297bce6770252f52c6fab4706843f3e0
Wadia Fatima <wadiafatima166@gmail.com>
2026-08-09 09:02:29 +0000

    Multiple Selection

--- Accordian/src/components/Accordian/index.jsx
+++ Accordian/src/components/Accordian/index.jsx
@@ -15,11 +15,18 @@ import './styles.css'
         setSelected(getCurrentId === selected ? null : getCurrentId);
     }
     function handleMultiSelection(getCurrentId){
-        setSelected(getCurrentId === selected ? null : getCurrentId);
+        let copyMulti = [...multiple]
+        const findIndexOfCurrentId = copyMulti.indexOf(getCurrentId)
+
+        console.log(findIndexOfCurrentId)
+        if(findIndexOfCurrentId === -1) copyMulti.push(getCurrentId)
+        else copyMulti.splice(findIndexOfCurrentId, 1)
+
+        setMultiple(copyMulti)
     }
 
 
-    console.log(selected);
+    console.log(selected, multiple);
     return (
         <div className='wrapper'>
             <h1 className="text-black-4xl">Accordian</h1>
@@ -30,14 +37,19 @@ import './styles.css'
                 {
                     data && data.length > 0 ? 
                     data.map(dataItem => <div className='item'>
-                        <div onClick = {enableMultiSelection ?() => handleMultiSelection (dataItem.id) : () => handleSingleSelection(dataItem.id)} className='title'>
+                        <div onClick = {
+                            enableMultiSelection 
+                            ? () => handleMultiSelection (dataItem.id) 
+                            : () => handleSingleSelection(dataItem.id)
+                          } 
+                        className='title'>
                             <h3>{dataItem.question}</h3>
                             <span>+</span>
                         </div>
                         {
-                            selected === dataItem.id ? 
-                            <div className='content'>{dataItem.answer}</div>
-                            : null
+                            enableMultiSelection 
+                            ? multiple.indexOf(dataItem.id) !== -1 && <div className='content'>{dataItem.answer}</div>
+                            : selected === dataItem.id && <div className='content'>{dataItem.answer}</div>
                         }
                     </div>)
                     : <div>No Data Found</div>
