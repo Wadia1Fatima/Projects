a47b4639ed030de6f71ea074d914a63351297a45
Wadia Fatima <wadiafatima166@gmail.com>
2026-08-11 13:44:59 +0500

    Complete ColorGenerator project

--- ColorGenerator/src/components/ClrGenerator/index.jsx
+++ ColorGenerator/src/components/ClrGenerator/index.jsx
@@ -1,20 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './style.css'
 
 
 export default function ClrGenerator(){
 
     const [typeOfColor, setTypeOfColor] = useState('hex');
-    const [color, setColor] = useState('#000000');
+    const [color, setColor] = useState('#ffffff');
+
+    function CreateHexColor(){
+        const hexColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        setColor(hexColor);
+    }
+
+    function CreateRgbColor(){
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        setColor(`rgb(${r}, ${g}, ${b})`);
+    }
+
+    function CreateColor(){
+        if(typeOfColor === 'hex'){
+            CreateHexColor();
+        } else {
+            CreateRgbColor();
+        }
+    }
+
+    useEffect(() => {
+        if(typeOfColor === 'rgb') CreateRgbColor()
+        else CreateHexColor()
+    },[typeOfColor])
 
 
     return(
         <div className="container" style = {{background: color}}>
-            <h1>Generate Random Color</h1>
+            <h1>Generate Random Colour</h1>
             <div className="buttons">
-                <button>HEX color</button>
-                <button>RGB color</button>
-                <button>Generate Color</button>
+                <button onClick={() => setTypeOfColor('hex')}>HEX colour</button>
+                <button onClick={() => setTypeOfColor('rgb')}>RGB colour</button>
+                <button onClick={CreateColor}>Generate Colour</button>
+            </div>
+            <div>
+                <h3>{typeOfColor === 'rgb' ? "RGB Colour" : "HEX Colour"}</h3>
+                <h3>{color}</h3>
             </div>
         </div>
     )
